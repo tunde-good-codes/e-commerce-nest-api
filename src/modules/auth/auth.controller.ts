53e5ff272c400e04b0f0bf68cca0102d68787a99
tundenewcodes <fasasipelumi1652@gmail.com>
2026-02-04 23:53:29 +0100

    auth-guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { RegisterDto } from "./dto/register.dto";
 import { AuthResponseDto } from "./dto/auth-response.dto";
+import { RefreshTokenGuard } from "./guards/refresh-token-guards";
 
 @Controller("auth")
 export class AuthController {
@@ -10,5 +11,12 @@ export class AuthController {
   @Post()
   async register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
     return await this.authService.register(registerDto);
+  }
+
+
+  @UseGuards(RefreshTokenGuard)
+  async refresh(@GetUser("id") userId: string): Promise<AuthResponseDto> {
+
+
   }
 }
